f3a26cbfb3c6f269c4978117136f53e2de901f98
Liwei Zhang <46981659+Jooooozlw@users.noreply.github.com>
2019-03-17 16:11:03 -0400

    Add files via upload

---
+++ lib/cross_validation_xgboost.R
@@ -0,0 +1,24 @@
+
+
+cv.function_xgboost <- function(X.train, y.train, modelvalues, K){
+  
+  n <- dim(y.train)[1]
+  n.fold <- floor(n/K)
+  s <- sample(rep(1:K, c(rep(n.fold, K-1), n-(K-1)*n.fold)))  
+  cv.error <- rep(NA, K)
+  
+  for (i in 1:K){
+    train.data <- X.train[s != i, ,]
+    train.label <- y.train[s != i, ,]
+    test.data <- X.train[s == i, ,]
+    test.label <- y.train[s == i, ,]
+    
+    #par <- list(depth=d)
+    fit <- train_xgboost(train.data, train.label, modelvalues)
+    pred <- test(fit, test.data)
+    pred <- pred$numericpred
+    cv.error[i] <- mean((pred - test.label)^2)  
+    
+  }			
+  return(c(mean(cv.error),sd(cv.error)))
+}
